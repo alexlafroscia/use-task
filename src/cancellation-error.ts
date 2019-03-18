887c2b3e487c832ddf8a30185ef7d69470ae0d74
Alex LaFroscia <alex@lafroscia.com>
2019-03-18 11:58:54 -0700

    feat: add helper for ignore cancellation errors
    Can be used to wrap an invocation of a task with a `try/catch` that swallows any cancellation errors, since you often don’t really care to explicitly handle task cancellation.

--- src/cancellation-error.ts
+++ src/cancellation-error.ts
@@ -2,6 +2,20 @@ export function isCancellationError(error: any): error is CancellationError {
   return error instanceof CancellationError;
 }
 
+export function ignoreCancellation<T extends (...args) => any>(callback: T) {
+  return async function(
+    ...args: Parameters<T>
+  ): Promise<ReturnType<T> | undefined> {
+    try {
+      return await callback(...args);
+    } catch (e) {
+      if (!isCancellationError(e)) {
+        throw e;
+      }
+    }
+  };
+}
+
 export default class CancellationError extends Error {
   constructor(message) {
     super(message);
