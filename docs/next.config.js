7f7080f1a075b0e97d38a531a3c23b9f682dabb9
Alex LaFroscia <alex@lafroscia.com>
2019-04-13 12:35:20 -0700

    chore: allow importing `use-task` in docs without compiling first
    - Update the Next.js webpack config to resolve `use-task` from `src`
    - Remove need to first compile library before starting docs server

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,34 @@
 /* eslint-env node */
 
+const path = require("path");
 const withTypescript = require("@zeit/next-typescript");
 
+const USE_TASK_PATH = path.resolve(__dirname, "../src");
+const USE_TASK_TSCONFIG = path.resolve(__dirname, "../tsconfig.json");
+
+function ensure(object, prop) {
+  object[prop] = object[prop] || {};
+}
+
 module.exports = withTypescript({
-  target: "serverless"
+  target: "serverless",
+  webpack(config) {
+    ensure(config, "resolve");
+    ensure(config.resolve, "alias");
+
+    config.resolve.alias["use-task"] = USE_TASK_PATH;
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      include: [USE_TASK_PATH],
+      use: {
+        loader: "awesome-typescript-loader",
+        options: {
+          configFileName: USE_TASK_TSCONFIG
+        }
+      }
+    });
+
+    return config;
+  }
 });
